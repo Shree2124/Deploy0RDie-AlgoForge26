1a8560aa631ec0d1eaf8800d0ff06701ab233bff
Shree2124 <shreealasande@gmail.com>
2026-03-28 14:24:15 +0530

    feat: predict and train logic api routes

---
+++ AI/app/routes/predict.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+import shutil
+import os
+from app.services.yolo_service import predict_image
+
+router = APIRouter()
+
+@router.post("/predict")
+async def predict(file: UploadFile = File(...)):
+
+    print(f"PREDICT ROUTE HIT! Received file: {file.filename}")
+
+
+    os.makedirs("temp", exist_ok=True)
+    file_path = f"temp/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    try:
+        print(f"Passed file to YOLO service: {file_path}")
+        detections = predict_image(file_path)
+        print("Result ",detections)
+    except FileNotFoundError as e:
+        print(f"ERROR: {e}")
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        print(f"ERROR: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+ 
+    print(f"PREDICTION COMPLETE! Detections found: {len(detections)}")
+    print(f"DETECTIONS: {detections}")
+
+
+    return {
+        "filename": file.filename,
+        "detections": detections
+    }
---
+++ AI/app/routes/train.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, BackgroundTasks
+from app.services.train_service import train_model
+
+router = APIRouter()
+
+@router.post("/train")
+def train(background_tasks: BackgroundTasks):
+    background_tasks.add_task(train_model)
+    return {"message": "Training started in the background"}
