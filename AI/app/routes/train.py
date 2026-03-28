8ca46c04264220cf4f80c70735419b3c654790f9
Krishna Choudhary <104185486+techy-krishna11568@users.noreply.github.com>
2026-03-28 18:09:02 +0530

    revert first commit

--- AI/app/routes/train.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, BackgroundTasks
-from app.services.train_service import train_model
-
-router = APIRouter()
-
-@router.post("/train")
-def train(background_tasks: BackgroundTasks):
-    background_tasks.add_task(train_model)
-    return {"message": "Training started in the background"}
